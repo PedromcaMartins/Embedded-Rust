8978855702404aa2e437537e3a686efd8a1b774b
Pedro Martins <pedromcamartins@tecnico.ulisboa.pt>
2024-03-21 15:13:46 +1100

    feat: it blinks!

--- NUCLEO-F413ZH/blink/src/main.rs
+++ NUCLEO-F413ZH/blink/src/main.rs
@@ -5,13 +5,15 @@ use cortex_m::asm::nop;
 use cortex_m_rt::entry;
 use rtt_target::{rtt_init_print, rprintln};
 use stm32f4xx_hal::{
-    pac::{self},
+    gpio::GpioExt, pac
 };
 extern crate panic_halt;
 
 #[entry]
 fn main() -> ! {
     let dp = pac::Peripherals::take().unwrap();
+    let gpiob = dp.GPIOB.split();
+    let mut green_led = gpiob.pb0.into_push_pull_output();
 
     rtt_init_print!();
     rprintln!("Hello, world!");
@@ -21,6 +23,7 @@ fn main() -> ! {
     loop {
         rprintln!("x = {}", x);
         x += 1;
+        green_led.toggle();
 
         for _ in 0..100_000 {
             nop();
