1bd271053108f09127f50d98ea87d1105cbf2dcc
Pedro Martins <pedromcamartins@tecnico.ulisboa.pt>
2024-03-23 23:23:56 +1100

    feat: fibocanni iterative

--- PC/07-generate-n-fibonacci/src/main.rs
+++ PC/07-generate-n-fibonacci/src/main.rs
@@ -1,4 +1,4 @@
-use std::{io, result};
+use std::{io, vec};
 
 fn main() {
 
@@ -22,16 +22,31 @@ fn main() {
             };
     
         let result: usize = fibonacci_recursive(n);
-
-        println!("Fibonacci value at index {n} is {result}");
+        println!("Fibonacci recursive value at index {n} is {result}");
+    
+        let result: usize = fibonacci_iterative(n);
+        println!("Fibonacci iterative value at index {n} is {result}");
         return;
     }
 }
 
 fn fibonacci_recursive(n: usize) -> usize {
     if n == 0 || n == 1 {
-        return n;
+        return 1;
     }
 
     return fibonacci_recursive(n - 2) + fibonacci_recursive(n - 1);
+}
+
+fn fibonacci_iterative(n: usize) -> usize {
+    let mut fibonacci_sequence: Vec<usize> = vec![1, 1];
+
+    for iteration in 2..n {
+        let fib_n_prev_2: usize = fibonacci_sequence[iteration - 2];
+        let fib_n_prev_1: usize = fibonacci_sequence[iteration - 1];
+        let fib_n_current: usize = fib_n_prev_2 + fib_n_prev_1;
+        fibonacci_sequence.push(fib_n_current);
+    }
+
+    return fibonacci_sequence.pop().expect("Last element of vector is invalid");
 }
