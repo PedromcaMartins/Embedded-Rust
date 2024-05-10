599f73245d1e0c2fa1484d006ba4c3e32914711c
Pedro Martins <pedromcamartins@tecnico.ulisboa.pt>
2024-05-10 21:35:44 +1000

    chore: tests added to t command and parser

--- Projects/IAED-P1-20-21/src/parser.rs
+++ Projects/IAED-P1-20-21/src/parser.rs
@@ -11,29 +11,76 @@ pub fn parser() -> Result<Command, &'static str> {
         return Err("Error reading line");
     }
 
-    parse_line(line.as_str())
+    parse_line(&line)
 }
 
 
 fn parse_line(line: &str) -> Result<Command, &'static str> {
-    let mut line: std::str::Chars = line.trim().chars();
+    let mut line = line.trim().chars();
     let command = match line.next() {
         Some(char) => char,
         None => return Err("Expected command"),
     };
 
-    let line = line.as_str().trim();
+    let args = line.as_str().trim();
 
     match command {
-        'q' => Err("quit"),
-        't' => t_command::parse_command(line),
+        'q' => q_command::parse_command(args),
+        't' => t_command::parse_command(args),
         _ => Err("Invalid command"),
     }
 }
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    
+    #[test]
+    #[should_panic(expected = "Expected command")]
+    fn parse_empty_line() {
+        let line = "";
+
+        let result = parse_line(line);
+
+        result.unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid command")]
+    fn parse_invalid_command() {
+        // s command does not exist
+        let line = "s string la la la";
+
+        let result = parse_line(line);
+
+        result.unwrap();
+    }
+
+    #[test]
+    fn parse_all_command() {
+        let line_q = "q";
+        let line_t = "t 2 description";
+        let line_l_1 = "l";
+        let line_l_2 = "l activity1 activity2";
+        let line_n = "n 2";
+        let line_u_1 = "u";
+        let line_u_2 = "u username";
+        let line_m = "m 2 username activity";
+        let line_d = "d activity";
+        let line_a_1 = "a";
+        let line_a_2 = "a activity";
+
+        assert!(parse_line(line_q).is_ok());
+        assert!(parse_line(line_t).is_ok());
+        assert!(parse_line(line_l_1).is_ok());
+        assert!(parse_line(line_l_2).is_ok());
+        assert!(parse_line(line_n).is_ok());
+        assert!(parse_line(line_u_1).is_ok());
+        assert!(parse_line(line_u_2).is_ok());
+        assert!(parse_line(line_m).is_ok());
+        assert!(parse_line(line_d).is_ok());
+        assert!(parse_line(line_a_1).is_ok());
+        assert!(parse_line(line_a_2).is_ok());
+    }
 }
--- Projects/IAED-P1-20-21/src/parser/t_command.rs
+++ Projects/IAED-P1-20-21/src/parser/t_command.rs
@@ -1,7 +1,7 @@
 use super::Command;
 
-pub fn parse_command(line: &str) -> Result<Command, &'static str> {
-    let (duration, description) = match line.split_once(' ') {
+pub fn parse_command(args: &str) -> Result<Command, &'static str> {
+    let (duration, description) = match args.split_once(' ') {
         Some((duration, description)) => (duration, description),
         None => return Err("Expected 't <duration> <description>'"),
     };
@@ -13,7 +13,6 @@ pub fn parse_command(line: &str) -> Result<Command, &'static str> {
 
     let description = description.trim().to_owned();
 
-    // TODO: verify if the values have the correct length, size, ...
     Ok(Command::NewTask{
         duration,
         description,
@@ -24,5 +23,73 @@ pub fn parse_command(line: &str) -> Result<Command, &'static str> {
 mod tests {
     use super::*;
 
-    
+    #[test]
+    fn parse_valid_command() {
+        let expected_duration = 10;
+        let expected_description = "finish rust project!";
+        let args = format!("{} {}", expected_duration, expected_description);
+
+        let result = parse_command(&args)
+            .unwrap_or_else(|err| panic!("Error: {}", err));
+
+        match result {
+            Command::NewTask { duration, description }
+                => {
+                    assert_eq!(expected_duration, duration);
+                    assert_eq!(expected_description, description);
+                },
+            _ => panic!("Parsed wrong command type. Expected Command::NewTask, but got {:?}", result),
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "'t <duration> <description>'")]
+    fn parse_no_arguments() {
+        let args = "";
+
+        let result = parse_command(args);
+
+        result.unwrap();
+    }
+
+    #[test]
+    #[should_panic(expected = "<duration> as integer")]
+    fn parse_no_duration() {
+        let expected_description = "finish rust project!";
+        let args = format!("{} {}", "", expected_description);
+
+        let result = parse_command(&args);
+
+        result.unwrap();
+    }
+
+    #[test]
+    fn parse_no_description() {
+        let expected_duration = 10;
+        let args = format!("{} {}", expected_duration, "");
+
+        let result = parse_command(&args)
+            .unwrap_or_else(|err| panic!("Error: {}", err));
+
+        match result {
+            Command::NewTask { duration, description }
+                => {
+                    assert_eq!(expected_duration, duration);
+                    assert_eq!(0, description.len());
+                },
+            _ => panic!("Parsed wrong command type. Expected Command::NewTask, but got {:?}", result),
+        }
+    }
+
+    #[test]
+    #[should_panic(expected = "<duration> as integer")]
+    fn parse_duration_not_a_number() {
+        let expected_duration = "0xa4e";
+        let expected_description = "finish rust project!";
+        let args = format!("{} {}", expected_duration, expected_description);
+
+        let result = parse_command(&args);
+
+        result.unwrap();
+    }
 }
