55971d852e07482ca39975940e439d1861eef79e
Pedro Martins <pedromcamartins@tecnico.ulisboa.pt>
2024-06-12 19:43:57 +1000

    chore: pool button changes blink speed

--- Tutorials Embassy/gpio_button_controlled_blinking/src/main.rs
+++ Tutorials Embassy/gpio_button_controlled_blinking/src/main.rs
@@ -3,9 +3,11 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::gpio::{Input, Level, Output, Speed, Pull};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _}; // global logger
+
+const BLINK_SPEEDS_MILLIS: [u64; 4] = [100, 200, 500, 1000];
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -13,11 +15,27 @@ async fn main(_spawner: Spawner) {
     info!("Hello, World!");
 
     let mut led = Output::new(p.PB7, Level::High, Speed::Low);
+    let button = Input::new(p.PC13, Pull::None);
+
+    let mut blink_speed_index = 0;
+    let mut blink_speed = get_new_blink_speed(&mut blink_speed_index);
 
     loop {
-        led.set_high();
-        Timer::after(Duration::from_millis(500)).await;
-        led.set_low();
-        Timer::after(Duration::from_millis(500)).await;
+        if button.is_high() {
+            blink_speed = get_new_blink_speed(&mut blink_speed_index);
+        }
+        
+        led.toggle();
+        Timer::after(Duration::from_millis(blink_speed)).await;
     }
+}
+
+fn get_new_blink_speed(blink_speed_index: &mut usize) -> u64 {
+    if *blink_speed_index == BLINK_SPEEDS_MILLIS.len() - 1 {
+        *blink_speed_index = 0;
+    }
+
+    *blink_speed_index += 1;
+
+    BLINK_SPEEDS_MILLIS[*blink_speed_index]
 }
