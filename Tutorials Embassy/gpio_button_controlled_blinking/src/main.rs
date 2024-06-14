f1f4ead9707f5640bfdd10b91593c050420c0dd3
Pedro Martins <pedromcamartins@tecnico.ulisboa.pt>
2024-06-14 20:19:54 +1000

    chore: Refactor main and initialize peripgerals function created

--- Tutorials Embassy/gpio_button_controlled_blinking/src/main.rs
+++ Tutorials Embassy/gpio_button_controlled_blinking/src/main.rs
@@ -5,53 +5,64 @@ use core::sync::atomic::{AtomicU32, Ordering};
 
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::{exti::ExtiInput, gpio::{AnyPin, Input, Level, Output, Pin, Pull, Speed}};
+use embassy_stm32::{exti::{AnyChannel, Channel, ExtiInput}, gpio::{AnyPin, Input, Level, Output, Pin, Pull, Speed}};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _}; // global logger
 
 const BLINK_SPEEDS_MS: [u32; 6] = [100, 150, 200, 250, 200, 150];
-
 static BLINK_MS: AtomicU32 = AtomicU32::new(0);
 
-#[embassy_executor::main]
-async fn main(spawner: Spawner) {
+
+struct Peripherals {
+    led_pin: AnyPin,
+    button_pin: AnyPin,
+    button_exti_channel: AnyChannel,
+}
+
+fn initialize_peripherals() -> Peripherals {
     let p = embassy_stm32::init(Default::default());
-    info!("Hello, World!");
 
-    let button = Input::new(p.PC13, Pull::None);
-    let mut button = ExtiInput::new(button, p.EXTI13);
-
-    let mut blink_speed_index = 0;
-    let mut blink_speed = get_new_blink_speed(&mut blink_speed_index);
-    BLINK_MS.store(blink_speed, Ordering::Relaxed);
-
-    spawner.spawn(led_task(p.PB7.degrade())).unwrap();
-
-    loop {
-        button.wait_for_rising_edge().await;
-
-        blink_speed = get_new_blink_speed(&mut blink_speed_index);
-        BLINK_MS.store(blink_speed, Ordering::Relaxed);
+    Peripherals {
+        led_pin: p.PB7.degrade(),
+        button_pin: p.PC13.degrade(),
+        button_exti_channel: p.EXTI13.degrade(),
     }
 }
 
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = initialize_peripherals();
+    info!("Hello, World!");
+
+    spawner.spawn(led_task(p.led_pin)).unwrap();
+    spawner.spawn(button_task(p.button_pin, p.button_exti_channel)).unwrap();
+}
+
 #[embassy_executor::task]
-async fn led_task(led_pin: AnyPin) {
+async fn led_task(led_pin: AnyPin) -> ! {
     let mut led = Output::new(led_pin, Level::High, Speed::Low);
 
     loop {
-        let blink_speed = BLINK_MS.load(Ordering::Relaxed);
         led.toggle();
+
+        let blink_speed = BLINK_MS.load(Ordering::Relaxed);
         Timer::after(Duration::from_millis(blink_speed.into())).await;   
     }
 }
 
-fn get_new_blink_speed(blink_speed_index: &mut usize) -> u32 {
-    if *blink_speed_index == BLINK_SPEEDS_MS.len() - 1 {
-        *blink_speed_index = 0;
+#[embassy_executor::task]
+async fn button_task(button_pin: AnyPin, button_exti_channel: AnyChannel) -> ! {
+    let button = Input::new(button_pin, Pull::None);
+    let mut button = ExtiInput::new(button, button_exti_channel);
+
+    let mut iter_blinking_speed = BLINK_SPEEDS_MS.iter().cycle();
+
+    loop {
+        if let Some(&speed) = iter_blinking_speed.next() {
+            BLINK_MS.store(speed, Ordering::Relaxed)
+        }
+
+        button.wait_for_rising_edge().await;
     }
-
-    *blink_speed_index += 1;
-
-    BLINK_SPEEDS_MS[*blink_speed_index]
 }
