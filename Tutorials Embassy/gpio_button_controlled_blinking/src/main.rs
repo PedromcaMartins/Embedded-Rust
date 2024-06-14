5c56066d7ef26112b9471e80a4ee119c8eda2321
Pedro Martins <pedromcamartins@tecnico.ulisboa.pt>
2024-06-14 19:52:58 +1000

    chore: Refactor button tirggered by interrupt

--- Tutorials Embassy/gpio_button_controlled_blinking/src/main.rs
+++ Tutorials Embassy/gpio_button_controlled_blinking/src/main.rs
@@ -1,41 +1,57 @@
 #![no_std]
 #![no_main]
 
+use core::sync::atomic::{AtomicU32, Ordering};
+
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Input, Level, Output, Speed, Pull};
+use embassy_stm32::{exti::ExtiInput, gpio::{AnyPin, Input, Level, Output, Pin, Pull, Speed}};
 use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _}; // global logger
 
-const BLINK_SPEEDS_MILLIS: [u64; 4] = [100, 200, 500, 1000];
+const BLINK_SPEEDS_MS: [u32; 6] = [100, 150, 200, 250, 200, 150];
+
+static BLINK_MS: AtomicU32 = AtomicU32::new(0);
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
     info!("Hello, World!");
 
-    let mut led = Output::new(p.PB7, Level::High, Speed::Low);
     let button = Input::new(p.PC13, Pull::None);
+    let mut button = ExtiInput::new(button, p.EXTI13);
 
     let mut blink_speed_index = 0;
     let mut blink_speed = get_new_blink_speed(&mut blink_speed_index);
+    BLINK_MS.store(blink_speed, Ordering::Relaxed);
+
+    spawner.spawn(led_task(p.PB7.degrade())).unwrap();
 
     loop {
-        if button.is_high() {
-            blink_speed = get_new_blink_speed(&mut blink_speed_index);
-        }
-        
-        led.toggle();
-        Timer::after(Duration::from_millis(blink_speed)).await;
+        button.wait_for_rising_edge().await;
+
+        blink_speed = get_new_blink_speed(&mut blink_speed_index);
+        BLINK_MS.store(blink_speed, Ordering::Relaxed);
     }
 }
 
-fn get_new_blink_speed(blink_speed_index: &mut usize) -> u64 {
-    if *blink_speed_index == BLINK_SPEEDS_MILLIS.len() - 1 {
+#[embassy_executor::task]
+async fn led_task(led_pin: AnyPin) {
+    let mut led = Output::new(led_pin, Level::High, Speed::Low);
+
+    loop {
+        let blink_speed = BLINK_MS.load(Ordering::Relaxed);
+        led.toggle();
+        Timer::after(Duration::from_millis(blink_speed.into())).await;   
+    }
+}
+
+fn get_new_blink_speed(blink_speed_index: &mut usize) -> u32 {
+    if *blink_speed_index == BLINK_SPEEDS_MS.len() - 1 {
         *blink_speed_index = 0;
     }
 
     *blink_speed_index += 1;
 
-    BLINK_SPEEDS_MILLIS[*blink_speed_index]
+    BLINK_SPEEDS_MS[*blink_speed_index]
 }
